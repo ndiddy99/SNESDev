efcf564e01e3f7553e3d72f0daaaa82fa7385a2d
Nathan Misner <nathan@thotpatrol.pw>
2019-01-22 22:19:13 -0500

    got l/r scrolling working

--- scroll.asm
+++ scroll.asm
@@ -15,7 +15,10 @@ HandleScroll:
 	beq EndHandleScroll ;if player's not moving, don't have to worry about scroll
 	cmp #$2 
 	beq MovingRight
-	
+	;issue: if the left and right boundaries are on 2 different screens, scroll screws up
+	;because of how i set scrollScreenAddr
+	;if going left and scroll & 1f goes up, that means you wrapped (so should change screen addy)
+	;same if going right and scroll & 1f goes down
 	MovingLeft:
 	lda scrollX
 	lsr
@@ -23,12 +26,11 @@ HandleScroll:
 	lsr
 	lsr ;leftmost onscreen column
 	dec a ;first offscreen column to the left
-	cmp scrollColumn
-	bcs EndHandleScroll
-	sta scrollColumn
 	and #$1f
-	cmp #$1f ;is rightmost tile on previous screen?
-	bne NotOnScreenBoundary
+	cmp scrollColumn
+	beq EndHandleScroll ; if it's the same, don't have to do anything
+	sta scrollColumn
+	bcc DoneMovementTests ;if it's less, normal screen movement
 		lda scrollScreenAddr
 		sec
 		sbc #$380 ;32 columns, 14 rows, 1 word per tile
@@ -43,13 +45,12 @@ HandleScroll:
 	lsr ;leftmost tile column on screen
 	clc
 	adc #$11 ;first offscreen tile
-	cmp scrollColumn ;beyond a new scroll boundary?
-	bcc EndHandleScroll
-	beq EndHandleScroll
-	sta scrollColumn
 	and #$1f
-	bne NotOnScreenBoundary
-		lda scrollScreenAddr
+	cmp scrollColumn ;beyond a new scroll boundary?
+	beq EndHandleScroll ;same? don't do anything
+	sta scrollColumn
+	bcs DoneMovementTests ;greater? tile pos didn't wrap
+		lda scrollScreenAddr ;otherwise, you've reached a new screen
 		clc
 		adc #$380 ;32 columns, 14 rows, 1 word per tile
 		sta scrollScreenAddr
@@ -57,7 +58,6 @@ HandleScroll:
 	
 	DoneMovementTests:
 		lda scrollColumn
-		and #$1f
 		asl ;words->bytes
 		pha ;>
 		clc
@@ -96,14 +96,12 @@ VramScrollCopy: ;run during vblank if there's new tile data to copy
 	sta PPUCTRL
 	a16
 	lda scrollColumn
-	and #$1f
 	asl
 	clc
 	adc #TilemapMirror ;copy from tilemap mirror to real tilemap
 	sta sourceAddr
 	
 	lda scrollColumn
-	and #$1f
 	sta PPUADDR ;set up where to write to in VRAM
 	
 	ldx #$e ;number of tiles to copy
